178be23a7f27c5150a9f8cbdf676b7942a76ec01
sebastianacunam <61118493+sebastianacunam@users.noreply.github.com>
2023-06-08 14:09:12 -0300

    se actualiza footer con icono y direccionamiento al gmail de citadina

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,15 +3,17 @@ import s from '../Footer/Footer.module.css'
 import instagram from '../../utils/instagram.png'
 import facebook from '../../utils/facebook.png'
 import whatsapp from '../../utils/whatsapp2.png'
+import mail from '../../utils/mail.png'
 
 
 export default function Footer() {
   return (
     <div className={s.container}>
         <div className={s.socialmedia}>
-            <p className={s.iconos}><a href="https://www.instagram.com/citadina.alojamiento/" target="_blank"><img className={s.icon} src={instagram} alt="img not found"/></a></p>
-            <p className={s.iconos}><a href="https://www.facebook.com/Citadinaalojamientotemporario?mibextid=LQQJ4d" target="_blank"><img className={s.icon} src={facebook} alt="img not found"/></a></p>
-            <p className={s.iconos}><a href="https://api.whatsapp.com/send?phone=91127338725" target="_blank"><img className={s.icon} src={whatsapp} alt="img not found"/></a></p>
+            <p className={s.iconos}><a href='mailto:alojamiento.citadina@gmail.com'><img className={s.iconmail} src={mail} alt='img not found'/></a></p>
+            <p className={s.iconos}><a href='https://www.instagram.com/citadina.alojamiento/' target='_blank'><img className={s.icon} src={instagram} alt='img not found'/></a></p>
+            <p className={s.iconos}><a href='https://www.facebook.com/Citadinaalojamientotemporario?mibextid=LQQJ4d' target='_blank'><img className={s.icon} src={facebook} alt='img not found'/></a></p>
+            <p className={s.iconos}><a href='https://api.whatsapp.com/send?phone=91127338725' target='_blank'><img className={s.icon} src={whatsapp} alt='img not found'/></a></p>
         </div>
         <div className={s.derechos}>
             <p>Todos los derechos reservados. Página web hecha por <a href="https://sebastianacunam.com" target="_blank">@sebastianacunam </a> </p>
