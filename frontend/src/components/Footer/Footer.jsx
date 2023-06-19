f5adc33c62ae380b8f5d52fab638ffccfe891074
sebastianacunam <61118493+sebastianacunam@users.noreply.github.com>
2023-06-19 13:59:44 -0300

    done

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,6 +4,8 @@ import instagram from '../../utils/instagram.png'
 import facebook from '../../utils/facebook.png'
 import whatsapp from '../../utils/whatsapp2.png'
 import mail from '../../utils/mail.png'
+import location from '../../utils/location.png'
+
 
 
 export default function Footer() {
@@ -13,7 +15,8 @@ export default function Footer() {
             <p className={s.iconos}><a href='mailto:alojamiento.citadina@gmail.com'><img className={s.iconmail} src={mail} alt='img not found'/></a></p>
             <p className={s.iconos}><a href='https://www.instagram.com/citadina.alojamiento/' target='_blank'><img className={s.icon} src={instagram} alt='img not found'/></a></p>
             <p className={s.iconos}><a href='https://www.facebook.com/Citadinaalojamientotemporario?mibextid=LQQJ4d' target='_blank'><img className={s.icon} src={facebook} alt='img not found'/></a></p>
-            <p className={s.iconos}><a href='https://api.whatsapp.com/send?phone=91127338725' target='_blank'><img className={s.icon} src={whatsapp} alt='img not found'/></a></p>
+            <p className={s.iconos}><a href='https://api.whatsapp.com/send?phone=351152788446' target='_blank'><img className={s.icon} src={whatsapp} alt='img not found'/></a></p>
+            <p className={s.iconos}><a href='https://maps.app.goo.gl/t7ZyspZcTv6Wxtd99?g_st=iw' target='_blank'><img className={s.icon} src={location} alt='img not found'/></a></p>
         </div>
         <div className={s.derechos}>
             <p>Todos los derechos reservados. Página web hecha por <a href="https://sebastianacunam.com" target="_blank">@sebastianacunam </a> </p>
--- frontend/src/components/WsIcon/WsIcon.jsx
+++ frontend/src/components/WsIcon/WsIcon.jsx
@@ -5,7 +5,7 @@ import s from '../WsIcon/WsIcon.module.css'
 
 export default function WsIcon() {
   return (
-    <a href="https://api.whatsapp.com/send?phone=91127338725" target="_blank" rel="noopener noreferrer" className={s.wsicon}>
+    <a href="https://api.whatsapp.com/send?phone=351152788446" target="_blank" rel="noopener noreferrer" className={s.wsicon}>
         <img className={s.wsImg} src={ws} alt="imt not found"/>
     </a>
   )
